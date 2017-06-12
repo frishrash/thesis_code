a3ade96a51593f879b992b17b58b9dd5a411961c
Gal Frishman <gal@il.ibm.com>
2017-06-12 18:19:05 +0300

    Removed special case for KMeansBal with partial number of classifiers

--- reports.py
+++ reports.py
@@ -117,9 +117,7 @@ def feasible_models(sigma=MAX_CLUSTERS_STD):
             ((result['Count'] == 10) & (result['Scaling'] == 'Min-max') &
              (result['Algo'] == 'NoSplit')) |
             ((result['Count'] == 8) & (result['Scaling'] == 'None') &
-             (result['Algo'] == 'NoSplit')) |
-            ((result['Count'] == 28) &
-             (result['Algo'].str.startswith('KMeansBal')))) & \
+             (result['Algo'] == 'NoSplit'))) & \
            (result['MODEL_MAX_RATIO'] < MAX_CLUSTERS_RATIO)
 
     return result[filt]
