0247f7436e52463c787704d6839a3b22af59d1da
Gal Frishman <gal@il.ibm.com>
2017-10-06 17:36:29 +0300

    Arguments support and reimplementation of feasibility report

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ Created on Sat May 20 17:40:33 2017
 import os
 import numpy as np
 import pandas as pd
+import argparse
 from sklearn.cluster import KMeans, Birch
 from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.ensemble import BaggingClassifier
@@ -152,8 +153,70 @@ def clustering_feasibility_report():
     csv_file.close()
 
 
-def eval_classifiers():
-    csv_file = open(CLASSIFIERS_REPORT, 'wb')
+def feasibile_clustering_models(report_file=CLUSTERS_REPORT):
+    csv_file = open(report_file, 'wb')
+    csvwriter = csv.writer(csv_file)
+
+    csvwriter.writerow(['Algorithm', 'Features', 'Dataset', 'Encoding',
+                        'Scaling', 'Max Std.', 'Max Ratio',
+                        'Avg. Split Time', 'Valid?', 'Other DS valid?'])
+    known_files = set()
+
+    for f in listdir(MODELS_DIR):
+        algo, features, dataset, encoding, scaling = splitext(f)[0].split('_')
+        file_name = join(MODELS_DIR, f)
+        if file_name in known_files:
+            continue
+        data = pickle.load(open(file_name, 'rb'))
+        max_std = np.max(map(lambda x: x['CLUSTERING_STD'], data))
+        max_ratio = np.max(map(lambda x: x['MINMAX_RATIO'], data))
+        avg_split_time = np.mean(map(lambda x: x['SPLIT_TIME'], data))
+        other_dataset = 'NSL Test+'
+        if dataset == 'NSL Test+':
+            other_dataset = 'NSL Train+ 20%'
+        other_f = '_'.join([algo, features, other_dataset, encoding,
+                            scaling])
+        other_f = join(MODELS_DIR, other_f + '.dmp')
+
+        is_model_valid = False
+        if is_feasible(data):
+            is_model_valid = True
+
+        is_other_valid = False
+        if not isfile(other_f):
+            print("Error, %s not found!" % other_f)
+            line = [algo, features, dataset, encoding, scaling, max_std,
+                    max_ratio, avg_split_time, is_model_valid, None]
+            csvwriter.writerow(line)
+        else:
+            known_files.add(other_f)
+            other_data = pickle.load(open(other_f, 'rb'))
+            if is_feasible(other_data):
+                is_other_valid = True
+
+            line = [algo, features, dataset, encoding, scaling, max_std,
+                    max_ratio, avg_split_time, is_model_valid, is_other_valid]
+            csvwriter.writerow(line)
+            max_std = np.max(map(lambda x: x['CLUSTERING_STD'], other_data))
+            max_ratio = np.max(map(lambda x: x['MINMAX_RATIO'], other_data))
+            avg_split_time = np.mean(map(lambda x: x['SPLIT_TIME'],
+                                         other_data))
+            line = [algo, features, other_dataset, encoding, scaling, max_std,
+                    max_ratio, avg_split_time, is_other_valid, is_model_valid]
+            csvwriter.writerow(line)
+    csv_file.close()
+
+
+def eval_classifiers(report_file=CLASSIFIERS_REPORT, classifiers=None,
+                     clusters_file=CLUSTERS_REPORT):
+    clfmap = {'NN': nn, 'DT': dt3, 'RF': rf3, 'MLP': mlp,
+              'SVM': svmlin}
+    classifiers_ = []
+    for clf in classifiers:
+        classifiers_.append(clfmap[clf])
+
+    clusters = pd.read_csv(clusters_file)
+    csv_file = open(report_file, 'wb')
     csvwriter = csv.writer(csv_file)
     header = ['Dataset', 'Encoding', 'Scaling', 'Algo', 'Features', 'K',
               'Split Sizes', 'Classifier', 'Classifier Info', 'K-fold',
@@ -166,56 +229,69 @@ def eval_classifiers():
     for a in labels:
         header.append("AUC %s" % a)
     csvwriter.writerow(header)
-    classifiers = [nn, dt3, rf3, mlp, svmlin]
-    # classifiers = [dt4, dt5, dt6]
 
-    for f in listdir(MODELS_DIR):
-        algo, features, dataset, encoding, scaling = splitext(f)[0].split('_')
-        data = pickle.load(open(join(MODELS_DIR, f), 'rb'))
-        if is_feasible(data):
-            ds_ = NSL(dataset, scaling=scaling, encoding=encoding)
-            for classifier in classifiers:
-                # Evaluate SVM only when min-max scaled (time constraint)
-                if classifier.name == 'SVM' and scaling != 'Min-max':
-                    continue
-                dump_file = "%s_%s.dmp" % (splitext(basename(f))[0],
-                                           classifier.name)
-                if (isfile(join(CLFS_DIR, dump_file))):
-                    print('Skipping %s, %s' % (f, classifier.name))
-                    continue
-                print('Working on %s, classifier %s' % (f, classifier.name))
-                ev = EvalClassifier(ds_, data, classifier, calc_prob=True)
+    for i, row in clusters.iterrows():
+        if row['Valid?'] is not True or row['Other DS valid?'] is not True:
+            continue
+        algo = row['Algorithm']
+        features = row['Features']
+        dataset = row['Dataset']
+        encoding = row['Encoding']
+        scaling = row['Scaling']
 
-                # If feasible (no errors during cross-validation)
-                if ev.eval():
-                    # Dump results
-                    pickle.dump(ev.results, open(join(CLFS_DIR, dump_file),
-                                                 'wb'))
+        model_file = '%s_%s_%s_%s_%s.dmp' % (algo, features, dataset, encoding,
+                                             scaling)
+        model_file = join(MODELS_DIR, model_file)
+        if not isfile(model_file):
+            print('Model %s was not found!' % model_file)
+            continue
 
-                    # Create report
-                    for i, res in enumerate(ev.results):
-                        line = [dataset, encoding, scaling, algo, features,
-                                data[i]['k'], ' - '.join(map(lambda x: str(x),
-                                                         data[i]['SPLIT_SIZES']
-                                                             )),
-                                classifier.name, classifier.info, ev.kfold,
-                                '%.2f' % res[EV_TIME_TRN],
-                                '%.2f' % res[EV_TIME_TST],
-                                '%.2f' % res[EV_FSCORE],
-                                '%.2f' % res[EV_PRE],
-                                '%.2f' % res[EV_REC]]
+        data = pickle.load(open(model_file, 'rb'))
+        ds_ = NSL(dataset, scaling=scaling, encoding=encoding)
+        for classifier in classifiers_:
+            # Evaluate SVM only when min-max scaled (time constraint)
+            #if classifier.name == 'SVM' and scaling != 'Min-max':
+            #    continue
 
-                        line = np.append(line, res[EV_CM].flatten())
-                        if EV_AUC in res:
-                            for lbl in NSL.standard_labels():
-                                line = np.append(line,
-                                                 '%.2f' % res[EV_AUC][lbl])
+            dump_file = '%s_%s_%s_%s_%s_%s.dmp' % (algo, features,
+                                                   dataset, encoding, scaling,
+                                                   classifier.name)
 
-                        csvwriter.writerow(line)
-                        csv_file.close()
-                        csv_file = open(CLASSIFIERS_REPORT, 'ab')
-                        csvwriter = csv.writer(csv_file)
+            if (isfile(join(CLFS_DIR, dump_file))):
+                print('Skipping %s' % dump_file)
+                continue
+            print('Working on %s' % dump_file)
+            ev = EvalClassifier(ds_, data, classifier, calc_prob=True)
 
+            # If feasible (no errors during cross-validation)
+            if ev.eval():
+                # Dump results
+                pickle.dump(ev.results, open(join(CLFS_DIR, dump_file),
+                                             'wb'))
+
+                # Create report
+                for i, res in enumerate(ev.results):
+                    line = [dataset, encoding, scaling, algo, features,
+                            data[i]['k'], ' - '.join(map(lambda x: str(x),
+                                                     data[i]['SPLIT_SIZES']
+                                                         )),
+                            classifier.name, classifier.info, ev.kfold,
+                            '%.2f' % res[EV_TIME_TRN],
+                            '%.2f' % res[EV_TIME_TST],
+                            '%.2f' % res[EV_FSCORE],
+                            '%.2f' % res[EV_PRE],
+                            '%.2f' % res[EV_REC]]
+
+                    line = np.append(line, res[EV_CM].flatten())
+                    if EV_AUC in res:
+                        for lbl in NSL.standard_labels():
+                            line = np.append(line,
+                                             '%.2f' % res[EV_AUC][lbl])
+
+                    csvwriter.writerow(line)
+                    csv_file.close()
+                    csv_file = open(report_file, 'ab')
+                    csvwriter = csv.writer(csv_file)
     csv_file.close()
 
 
@@ -276,7 +352,50 @@ def feasible_models_output():
                          file_name=os.path.join(GRAPHS_DIR,
                                                 'baseline-dt-comparison.png'))
 
-create_clustering_models()
-# clustering_feasibility_report()
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest="command", help='sub-command help')
+subparsers.add_parser('CM', help='Create clustering models')
+parser_fr = subparsers.add_parser('FR', help='Create feasibility report')
+parser_fr.add_argument('-O', dest='output', default=CLUSTERS_REPORT,
+                       help='Output file')
+parser_ec = subparsers.add_parser('EC', help='Evaluate classifiers')
+parser_ec.add_argument('-n', '--NN', dest='classifiers', action='append_const',
+                       const='NN', help='Evaluate 1-Nearest-Neighbor')
+parser_ec.add_argument('-d', '--DT', dest='classifiers', action='append_const',
+                       const='DT', help='Evaluate Decision Tree')
+parser_ec.add_argument('-r', '--RF', dest='classifiers', action='append_const',
+                       const='RF', help='Evaluate Random Forest')
+parser_ec.add_argument('-m', '--MLP', dest='classifiers',
+                       action='append_const', const='MLP',
+                       help='Evaluate Multilayer Perceptron')
+parser_ec.add_argument('-s', '--SVM', dest='classifiers',
+                       action='append_const', const='SVM', help='Evaluate SVM')
+parser_ec.add_argument('-O', dest='output', default=CLASSIFIERS_REPORT,
+                       help='Output file')
+parser_ec.add_argument('-C', dest='clusters_report', default=CLUSTERS_REPORT,
+                       help='Output file')
+subparsers.add_parser('PO', help='Plot output')
+
+
+args = parser.parse_args()
+if args.command == 'EC' and not args.classifiers:
+    parser.error("At least one classifier must be specified")
+
+if args.command == 'CM':
+    create_clustering_models()
+elif args.command == 'FR':
+    feasibile_clustering_models(report_file=args.output)
+elif args.command == 'PO':
+    feasible_models_output()
+elif args.command == 'EC':
+    eval_classifiers(report_file=args.output, classifiers=args.classifiers,
+                     clusters_file=args.clusters_report)
+    
+#if not args.mode:
+#    parser.error("One of --process or --upload must be given")
+
+# create_clustering_models()
+#feasibile_clustering_models()
+#clustering_feasibility_report()
 # eval_classifiers()
 # feasible_models_output()
